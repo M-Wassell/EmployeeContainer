f3b3b4096a78c95906fe2487828ffd4dfc9a90d9
M-Wassell <55836302+M-Wassell@users.noreply.github.com>
2021-01-22 01:31:02 +0000

    Add project files.

---
+++ EmployeeContainer/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeContainer.Models;
+using EmployeeContainer.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeContainer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployee();
+            return View(model);
+        }
+        public ViewResult Details(int id)
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(id),
+                PageTitle = "Employee Details"
+            };
+            
+            return View(homeDetailsViewModel);
+        }
+    }
+}
---
+++ EmployeeContainer/Models/MockEmployeeRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeContainer.Models
+{
+
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){ Id = 1, Name = "Michael", Email = "mike1791.mw@gmail.com"},
+                new Employee(){ Id = 2, Name = "Jamie", Email = "Jamie@gmail.com"},
+                new Employee(){ Id = 3, Name = "Megan", Email = "Megan@gmail.com"}
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
